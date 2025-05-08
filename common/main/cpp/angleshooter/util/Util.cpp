8db01d95da43c7bf391faae2cd171d85738e0e36
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2025-05-08 03:24:17 +0100

    Adjust Util Methods

--- common/main/cpp/angleshooter/util/Util.cpp
+++ common/main/cpp/angleshooter/util/Util.cpp
@@ -37,12 +37,12 @@ int Util::randomInt(int exclusiveMax) {
 }
 
 float Util::randomFloat() {
-	std::uniform_real_distribution<float> distribution(0, 1);
+	std::uniform_real_distribution distribution(0.f, 1.f);
 	return distribution(randomEngine);
 }
 
 float Util::randomFloat(float min, float max) {
-	std::uniform_real_distribution<float> distribution(min, max);
+	std::uniform_real_distribution distribution(min, max);
 	return distribution(randomEngine);
 }
 
@@ -64,20 +64,27 @@ std::string Util::toRoundedString(double value, int decimals) {
 }
 
 uint16_t Util::swapBytes16(uint16_t inData) {
-	return (inData >> 8 | inData << 8);
+	return (
+		(inData >> 8 & 0xFF) |
+		(inData << 8 & 0xFF << 8));
 }
 
 uint32_t Util::swapBytes32(uint32_t inData) {
-	return (inData >> 24 & 0x000000ff) | (inData >> 8 & 0x0000ff00) | (inData << 8 & 0x00ff0000) | (inData << 24 & 0xff000000);
+	return (
+		(inData >> 24 & 0xFF) |
+		(inData >>  8 & 0xFF << 8) |
+		(inData <<  8 & 0xFF << 16) |
+		(inData << 24 & 0xFF << 24));
 }
 
 uint64_t Util::swapBytes64(uint64_t inData) {
-	return (inData >> 56 & 0x00000000000000ff) |
-		(inData >> 40 & 0x000000000000ff00) |
-		(inData >> 24 & 0x0000000000ff0000) |
-		(inData >> 8 & 0x00000000ff000000) |
-		(inData << 8 & 0x000000ff00000000) |
-		(inData << 24 & 0x0000ff0000000000) |
-		(inData << 40 & 0x00ff000000000000) |
-		(inData << 56 & 0xff00000000000000);
+	return (
+		(inData >> 56 & 0xFF) |
+		(inData >> 40 & 0xFF << 8) |
+		(inData >> 24 & 0xFF << 16) |
+		(inData >>  8 & 0xFF << 24) |
+		(inData <<  8 & 0xFF00000000) |
+		(inData << 24 & 0xFF00000000 << 8) |
+		(inData << 40 & 0xFF00000000 << 16) |
+		(inData << 56 & 0xFF00000000 << 24));
 }
